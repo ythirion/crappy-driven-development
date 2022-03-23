9a04261483070b8fe7c701df71292ed134eca981
Yoan Thirion <yoan.thirion@gmail.com>
2022-03-23 18:58:18 +0100

    small cleanup

--- src/scala/conway/src/main/scala/conway/ConwaySequence.scala
+++ src/scala/conway/src/main/scala/conway/ConwaySequence.scala
@@ -2,16 +2,16 @@ package conway
 
 object ConwaySequence {
   def printAt(originalNumber: Int, line: Int): String = {
-    var result: String = originalNumber.toString
-    (1 until line).foreach(_ => result = generateNextLine(result))
+    var result = s"$originalNumber"
+    (1 until line).foreach(_ => result = nextLine(result))
     result
   }
 
-  private def generateNextLine(line: String): String = {
+  private def nextLine(line: String): String = {
     val numbers = splitLine(line)
     var count = 0
     var currentNumber: String = numbers.head
-    var result = ""
+    var nextLine = ""
 
     def resetCurrentNumber(number: String): Unit = {
       count = 1
@@ -22,13 +22,13 @@ object ConwaySequence {
       .foreach(number => {
         if (number == currentNumber) count += 1
         else {
-          result += lookAndSay(count, currentNumber)
+          nextLine += lookAndSay(count, currentNumber)
           resetCurrentNumber(number)
         }
       })
 
-    result += lookAndSay(count, currentNumber)
-    cleanResult(result)
+    nextLine += lookAndSay(count, currentNumber)
+    cleanResult(nextLine)
   }
 
   private def cleanResult(result: String) = {
